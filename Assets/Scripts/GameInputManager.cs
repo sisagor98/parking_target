0cc8cf87c5de4974b398207ed0b8b65f92af8c0d
Md. Shariful Islam <42964022+sisagor98@users.noreply.github.com>
2023-06-04 12:13:25 +0600

    parking order

---
+++ Assets/Scripts/GameInputManager.cs
@@ -0,0 +1,81 @@
+using System.Net.Mime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class GameInputManager : MonoBehaviour
+{
+    public static GameInputManager Instance; 
+    
+    public GameObject selectedPlayer = null;
+    private Camera m_Camera;
+    private Vector2 startTouchPosition;
+    private Vector2 currentPosition;
+
+    private Player selectedPlayerInstance;
+    private Transform targetPoint;
+    public int totallArrow=5;
+
+
+    void Awake()
+    {
+        m_Camera = Camera.main;
+       Instance = this;
+      
+    }
+    void Update() { 
+  
+        Swipe();
+      
+    }
+
+
+    void Swipe()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            startTouchPosition = Input.mousePosition;
+            Ray ray = m_Camera.ScreenPointToRay(startTouchPosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.CompareTag("Player"))
+                {
+                    selectedPlayer = hit.transform.gameObject;
+                    selectedPlayerInstance = selectedPlayer.GetComponent<Player>();
+                    selectedPlayerInstance.isSelectedPlayer = true;
+                    //selectedArrowInstance.rigidBody.isKinematic = false;
+                    selectedPlayerInstance.SetMovementandDirection(true);
+                }
+            }
+        }
+
+        if (Input.GetMouseButton(0) && selectedPlayer != null)
+        {
+            currentPosition = Input.mousePosition;
+            Vector2 Distance = currentPosition - startTouchPosition;
+
+
+            if (Input.GetMouseButtonUp(0))
+            {
+
+                if (selectedPlayer != null)
+                {
+                    bool shouldPlay = selectedPlayerInstance.shallPlay;
+                    selectedPlayerInstance.shallPlay = !shouldPlay;
+
+                    print("Tap detected");
+                }
+
+
+
+            }
+            
+
+        }
+
+    }
+   
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.Serialization;
+
+public class Player : MonoBehaviour
+{
+
+    public Rigidbody rigidBody;
+    [SerializeField] private float movementSpeed = 0;
+    private Vector3 movementDirection;
+
+    public bool shallMove = false;
+    public bool isSelectedPlayer = false;
+    public bool shallPlay = false;
+
+    public Transform arrowPosition;
+   [HideInInspector] public float enemyDistance;
+
+
+    public Transform[] waypoints;
+    public float speed;
+    private int currentWaypointIndex = 0;
+
+
+
+    void Awake()
+    {
+      //  rigidBody = GetComponent<Rigidbody>();
+        
+    }
+    private void Start()
+    {
+        Application.targetFrameRate = 60;
+    }
+    void Update()
+    {
+        if (shallMove)
+        {
+            PlayerMoveWay();
+           // MoveArrow();
+        }
+    }
+
+    private void MoveArrow()
+    {
+        if (movementDirection != null)
+        {
+            rigidBody.velocity = movementDirection * movementSpeed;
+            //if(rigidBody.velocity.magnitude < 5f )
+            //{
+            //    rigidBody.velocity = movementDirection * movementSpeed;
+            //   // print(rigidBody.velocity);
+            //}
+        }
+    }
+
+    public void SetMovementandDirection(bool moveArrow)
+    {
+        shallMove = moveArrow;
+    }
+    
+
+    public void PlayerMoveWay()
+    {
+        
+        Vector3 waypointPosition = waypoints[currentWaypointIndex].position;
+
+       
+        Vector3 direction = waypointPosition - transform.position;
+        transform.position += direction * speed * Time.deltaTime;
+
+     
+        if (Vector3.Dot(direction, direction) < 0.01f)
+        {
+            
+            currentWaypointIndex++;
+
+            if (currentWaypointIndex >= waypoints.Length)
+            {
+                currentWaypointIndex = 0;
+            }
+        }
+    }
+   
+
+
+
+    //public void OnCollisionFeedback()
+    //{
+    //    if (isSelectedArrow)
+    //    {
+    //        rigidBody.isKinematic = true;
+    //        shallMove = false;
+    //        isSelectedArrow = false;
+    //        GameInputManager.Instance.selectedArrow = null;
+    //        Vector3 hitPosition = transform.position;
+    //        rigidBody.velocity = Vector3.zero;
+    //        rigidBody.angularVelocity = Vector3.zero;
+    //        Vector3 newPos = hitPosition - (movementDirection * 0.6f);
+     
+        
+    //        transform.DOMove(newPos,0.2f).OnComplete(() =>
+    //            {
+    //                //To Add more collision effeft;
+    //            }
+    //        );
+    //    }
+    //    else
+    //    {
+            
+    //        // var rotation = transform.InverseTransformDirection(movementDirection);
+    //        // (rotation.x, rotation.z) = (-rotation.z, rotation.x);
+    //        // transform.DOPunchRotation(rotation * 7f, 0.3f, 8).SetId(transform);
+    //        transform.DOShakePosition(0.25f,0.2f,2);
+    //    }
+    //}
+
+    public void StopPhysicsBasedMovement()
+    {
+        rigidBody.isKinematic = true;
+        shallMove = false;
+        rigidBody.velocity = Vector3.zero;
+        rigidBody.angularVelocity = Vector3.zero;
+        isSelectedPlayer = false;
+    }
+
+
+
+
+}
